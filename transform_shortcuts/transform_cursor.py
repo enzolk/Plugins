377efeefe522aa2fee52b610171ec31d6793197f
enzolk <39864244+enzolk@users.noreply.github.com>
2026-03-05 16:14:33 +0100

    Create complete transform shortcuts addon

--- transform_shortcuts/transform_cursor.py
+++ transform_shortcuts/transform_cursor.py
@@ -1,19 +1,9 @@
-bl_info = {
-    "name": "Transform Shortcut: Cursor",
-    "author": "OpenAI",
-    "version": (1, 0, 0),
-    "blender": (5, 0, 0),
-    "location": "View3D > Sidebar > Tool > Transform Shortcuts",
-    "description": "Set transform orientation and pivot point to 3D Cursor",
-    "category": "3D View",
-}
-
 import bpy
 
 
 class VIEW3D_OT_set_cursor_transform(bpy.types.Operator):
     bl_idname = "view3d.set_cursor_transform"
-    bl_label = "Set Cursor Transform"
+    bl_label = "Transform Cursor"
     bl_description = "Set Transform Orientation to Cursor and Pivot Point to 3D Cursor"
     bl_options = {'REGISTER', 'UNDO'}
 
@@ -28,22 +18,7 @@ class VIEW3D_OT_set_cursor_transform(bpy.types.Operator):
         return {'FINISHED'}
 
 
-class VIEW3D_PT_transform_shortcuts_cursor(bpy.types.Panel):
-    bl_label = "Transform Shortcuts"
-    bl_idname = "VIEW3D_PT_transform_shortcuts_cursor"
-    bl_space_type = 'VIEW_3D'
-    bl_region_type = 'UI'
-    bl_category = "Tool"
-
-    def draw(self, context):
-        layout = self.layout
-        layout.operator(VIEW3D_OT_set_cursor_transform.bl_idname, icon='PIVOT_CURSOR')
-
-
-classes = (
-    VIEW3D_OT_set_cursor_transform,
-    VIEW3D_PT_transform_shortcuts_cursor,
-)
+classes = (VIEW3D_OT_set_cursor_transform,)
 
 
 def register():
@@ -54,7 +29,3 @@ def register():
 def unregister():
     for cls in reversed(classes):
         bpy.utils.unregister_class(cls)
-
-
-if __name__ == "__main__":
-    register()
