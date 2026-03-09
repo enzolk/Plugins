18a2bda4646c13dd99982d8f45476b68807bdfe3
enzolk <39864244+enzolk@users.noreply.github.com>
2026-03-09 11:15:53 +0100

    Add PySide6/shiboken6 fallback for Maya 2025+

---
+++ maya_shortcut_monitor/__init__.py
@@ -0,0 +1,5 @@
+"""Maya shortcut monitor package."""
+
+from .shortcut_tracker import MayaShortcutTracker, start_tracker, stop_tracker
+
+__all__ = ["MayaShortcutTracker", "start_tracker", "stop_tracker"]
