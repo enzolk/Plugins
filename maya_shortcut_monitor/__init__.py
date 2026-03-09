a7021a61207e54a5d39b04dff751240faefb8bb0
enzolk <39864244+enzolk@users.noreply.github.com>
2026-03-09 11:15:32 +0100

    Revert "Add maya_shortcut_monitor package to track and persist Maya hotkey usage"

--- maya_shortcut_monitor/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Maya shortcut monitor package."""
-
-from .shortcut_tracker import MayaShortcutTracker, start_tracker, stop_tracker
-
-__all__ = ["MayaShortcutTracker", "start_tracker", "stop_tracker"]
